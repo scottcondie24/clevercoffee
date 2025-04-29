ecdbd3a6a24894a8a4959b6edd0fe8c7af682f2e
Scott Condie <scottcondie24+github@gmail.com>
2025-04-29 10:41:06 +1000

    Modified to mirror LoQue90 rm-brewdetection fork with new changes

--- src/steamHandler.h
+++ src/steamHandler.h
@@ -3,7 +3,6 @@
  *
  * @brief Handler for digital steam switch
  */
-#include <hardware/pinmapping.h>
 
 uint8_t currStateSteamSwitch;
 
@@ -20,20 +19,6 @@ void checkSteamSwitch() {
             // if activated via web interface then steamFirstON == 1, prevent override
             if (steamSwitchReading == LOW && !steamFirstON) {
                 steamON = 0;
-            }
-
-            // monitor QuickMill thermoblock steam-mode
-            if (machine == QuickMill) {
-                if (steamQM_active == true) {
-                    if (checkSteamOffQM() == true) { // if true: steam-mode can be turned off
-                        steamON = 0;
-                        steamQM_active = false;
-                        lastTimeOptocouplerOn = 0;
-                    }
-                    else {
-                        steamON = 1;
-                    }
-                }
             }
         }
         else if (STEAMSWITCH_TYPE == Switch::MOMENTARY) {
